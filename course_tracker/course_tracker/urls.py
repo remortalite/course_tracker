8b09f0f788772aaadc2fc0fcb12a706c028de5af
Rem M. <morrig407@gmail.com>
2024-09-16 23:35:26 +0700

    Add template menu

--- course_tracker/course_tracker/urls.py
+++ course_tracker/course_tracker/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from .views import LoginUserView, LogoutUserView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
 
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
