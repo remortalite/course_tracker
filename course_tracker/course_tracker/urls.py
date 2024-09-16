f7d0fb48478103d2a462e291930cf9c75f28eecc
Rem M. <morrig407@gmail.com>
2024-09-16 23:00:55 +0700

    Add login/logout views

--- course_tracker/course_tracker/urls.py
+++ course_tracker/course_tracker/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import LoginUserView, LogoutUserView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('login/', LoginUserView.as_view(), name='login'),
+    path('logout/', LogoutUserView.as_view(), name='logout'),
+
 
     path('users/', include('users.urls')),
 ]
