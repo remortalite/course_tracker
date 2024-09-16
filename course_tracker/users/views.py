a7f7215b19e19a129fc5e8a7c0f6e17116b2aaab
Rem M. <morrig407@gmail.com>
2024-09-16 22:50:27 +0700

    Add extra_context to form.html

--- course_tracker/users/views.py
+++ course_tracker/users/views.py
@@ -1,4 +1,6 @@
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 from .forms import UserCreateForm
@@ -8,3 +10,10 @@ class CreateUserView(CreateView):
     model = User
     form_class = UserCreateForm
     template_name = 'form.html'
+    next_page = reverse_lazy('home')
+
+    extra_context = {
+        'title': _('Create user'),
+        'header': _('Create user'),
+        'button_name': _('Sign up'),
+    }
