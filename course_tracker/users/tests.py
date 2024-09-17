c15fa21455d4cad6d5292323c584f77d1fb616d8
Rem M. <morrig407@gmail.com>
2024-09-17 20:00:29 +0700

    Add tests for login

--- course_tracker/users/tests.py
+++ course_tracker/users/tests.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.test import TestCase
+from django.urls import reverse_lazy
 
-# Create your tests here.
+
+class UsersTest(TestCase):
+    fixtures = ['sample.json']
+    def test_login_get(self):
+        response = self.client.get(reverse_lazy('login'))
+        self.assertEqual(response.status_code, HttpResponse.status_code)
+        self.assertIn(b'Login', response.content)
+
+    def test_login_post(self):
+        response = self.client.post(reverse_lazy('login'),
+                                    follow=True,
+                                    data={
+                                        'username': 'test_user',
+                                        'password': 'TQ3XxstZdj1CJf',
+                                    })
+        self.assertEqual(response.status_code, HttpResponse.status_code)
+        self.assertIn(b'Login success', response.content)
+        self.assertRedirects(response, reverse_lazy('home'))
