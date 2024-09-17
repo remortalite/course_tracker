3a6fb7aa4c58b0805c6e9435e1f2d8a1b9db80ec
Rem M. <morrig407@gmail.com>
2024-09-17 20:23:54 +0700

    Add tests for logout

--- course_tracker/users/tests.py
+++ course_tracker/users/tests.py
@@ -21,3 +21,11 @@ class UsersTest(TestCase):
         self.assertEqual(response.status_code, HttpResponse.status_code)
         self.assertIn(b'Login success', response.content)
         self.assertRedirects(response, reverse_lazy('home'))
+
+    def test_logout(self):
+        self.client.login(username='test_user', password='TQ3XxstZdj1CJf')
+
+        response = self.client.post(reverse_lazy('logout'), follow=True)
+        self.assertEqual(response.status_code, HttpResponse.status_code)
+        self.assertRedirects(response, reverse_lazy('home'))
+        self.assertIn(b'Logged out', response.content)
