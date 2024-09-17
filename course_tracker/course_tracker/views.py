828414b4c46e97bda408510287309587f7581e0c
Rem M. <morrig407@gmail.com>
2024-09-17 20:24:10 +0700

    Add flash message foe logout

--- course_tracker/course_tracker/views.py
+++ course_tracker/course_tracker/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.messages.views import SuccessMessageMixin
 from django.utils.translation import gettext_lazy as _
+from django.contrib import messages
 
 from django.urls import reverse_lazy
 
@@ -21,3 +22,9 @@ class LoginUserView(SuccessMessageMixin, LoginView):
 
 class LogoutUserView(LogoutView):
     next_page = reverse_lazy('home')
+
+    success_message = _('Logged out')
+
+    def post(self, request, *args, **kwargs):
+        messages.info(request, self.success_message)
+        return super().post(request, *args, **kwargs)
