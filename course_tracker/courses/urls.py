74919cc4d8f06b94a5e5dc4586e998c267314513
Rem M. <morrig407@gmail.com>
2024-10-03 21:25:25 +0700

    Add Course creation view
    Add view CourseCreateView. Add url for creation to urlpatterns with name
    'courses.create'

--- course_tracker/courses/urls.py
+++ course_tracker/courses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CoursesListView
+from .views import CoursesListView, CourseCreateView
 
 
 urlpatterns = [
     path('', CoursesListView.as_view(), name='courses.home'),
+    path('create/', CourseCreateView.as_view(), name='courses.create'),
 ]
--- course_tracker/courses/views.py
+++ course_tracker/courses/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from django.utils.translation import gettext_lazy as _
+from django.urls import reverse_lazy
 
 from .models import Course
 from users.mixins import LoginRequiredWithMsgMixin
@@ -12,4 +13,18 @@ class CoursesListView(LoginRequiredWithMsgMixin, ListView):
 
     extra_context = {
         "header": "Courses",
+    }
+
+
+class CourseCreateView(LoginRequiredWithMsgMixin, CreateView):
+    model = Course
+    success_url = reverse_lazy('courses.home')
+    template_name = 'form.html'
+    fields = '__all__'
+
+    message_no_auth = _('Please log in')
+
+    extra_context = {
+        'header': _('Create course'),
+        'button_name': _('Create'),
     }
