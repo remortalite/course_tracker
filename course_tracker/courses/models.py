a28e2edb2c480ed5680b06172083829d24491c47
Rem M. <morrig407@gmail.com>
2024-10-04 12:25:58 +0700

    Add author field for Course  as foreign key for User

--- course_tracker/courses/models.py
+++ course_tracker/courses/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from users.models import User
+
 
 class Course(models.Model):
     name = models.CharField(verbose_name=_("Name"), max_length=128)
     url = models.CharField(verbose_name=_("URL"), max_length=255)
+
+    author = models.ForeignKey(to=User, verbose_name=_('Author'), on_delete=models.PROTECT)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
