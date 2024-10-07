08ab1645e048bcc36c690c913dd9be50eef0cec7
Rem M. <morrig407@gmail.com>
2024-10-07 23:17:21 +0700

    Fix linter errors

--- course_tracker/tasks/models.py
+++ course_tracker/tasks/models.py
@@ -12,10 +12,15 @@ class Task(models.Model):
     )
 
     name = models.CharField(verbose_name=_('Name'), max_length=128)
-    description = models.TextField(verbose_name=_('Description'), null=True, blank=True)
-    link = models.CharField(verbose_name=_('Link'), max_length=255, null=True, blank=True)
-    status = models.CharField(verbose_name=_('Status'), max_length=12, choices=STATUS_CHOICES)
-    course = models.ForeignKey(verbose_name=_('Course'), to=Course, on_delete=models.PROTECT, null=True, blank=True, related_name='tasks')
+    description = models.TextField(verbose_name=_('Description'),
+                                   null=True, blank=True)
+    link = models.CharField(verbose_name=_('Link'), max_length=255,
+                            null=True, blank=True)
+    status = models.CharField(verbose_name=_('Status'), max_length=12,
+                              choices=STATUS_CHOICES)
+    course = models.ForeignKey(verbose_name=_('Course'), to=Course,
+                               on_delete=models.PROTECT, null=True,
+                               blank=True, related_name='tasks')
 
     def __str__(self):
         return self.name
--- course_tracker/tasks/views.py
+++ course_tracker/tasks/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse_lazy
