ed1f34c49d12cd5d738d54d239ab45357f842cc0
Rem M. <morrig407@gmail.com>
2024-10-07 22:13:58 +0700

    Model Task created

--- course_tracker/tasks/models.py
+++ course_tracker/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Task(models.Model):
+    STATUS_CHOICES = (
+        ("INPROGRESS", _("In progress")),
+        ("FINISHED", _("Finished")),
+        ("FREEZED", _("Freezed")),
+    )
+
+    name = models.CharField(verbose_name=_('Name'), max_length=128)
+    description = models.TextField(verbose_name=_('Description'), null=True, blank=True)
+    link = models.CharField(verbose_name=_('Link'), max_length=255, null=True, blank=True)
+    status = models.CharField(verbose_name=_('Status'), max_length=12, choices=STATUS_CHOICES)
