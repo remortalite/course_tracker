b2409f94ae92e6144d09248466e6787f06dd55b5
Rem M. <morrig407@gmail.com>
2024-10-07 22:16:45 +0700

    Add Task foreign key  to Course

--- course_tracker/tasks/models.py
+++ course_tracker/tasks/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from courses.models import Course
+
 
 class Task(models.Model):
     STATUS_CHOICES = (
@@ -13,3 +15,4 @@ class Task(models.Model):
     description = models.TextField(verbose_name=_('Description'), null=True, blank=True)
     link = models.CharField(verbose_name=_('Link'), max_length=255, null=True, blank=True)
     status = models.CharField(verbose_name=_('Status'), max_length=12, choices=STATUS_CHOICES)
+    course = models.ForeignKey(verbose_name=_('Course'), to=Course, on_delete=models.PROTECT, null=True, blank=True)
