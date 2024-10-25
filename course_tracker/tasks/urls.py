3dfeddf782e31e6ec85c916d5de8622fc736a255
Rem M. <morrig407@gmail.com>
2024-10-25 22:18:06 +0700

    Add broken link at Task detail view

--- course_tracker/tasks/urls.py
+++ course_tracker/tasks/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', TaskListView.as_view(), name='tasks.home'),
     path('create/', TaskCreateView.as_view(), name='tasks.create'),
     path('<int:pk>/', TaskDetailView.as_view(), name='tasks.detail'),
-    path('<int:pk>/update', TaskUpdateView.as_view(), name='tasks.update'),
-    path('<int:pk>/delete', TaskDeleteView.as_view(), name='tasks.delete'),
+    path('<int:pk>/update/', TaskUpdateView.as_view(), name='tasks.update'),
+    path('<int:pk>/delete/', TaskDeleteView.as_view(), name='tasks.delete'),
 ]
